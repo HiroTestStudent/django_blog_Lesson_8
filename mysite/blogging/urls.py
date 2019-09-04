4f0a7595301434f1d31ad63b7e896cef44033cb6
Hiroyuki Takechi <hiroyukitakechi@gmail.com>
2019-09-04 15:41:32 -0700

    WIP Sept 4

--- mysite/blogging/urls.py
+++ mysite/blogging/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from blogging.views import stub_view
 
-urlpatterns = [
-    path('', stub_view, name="blog_index"),
+from blogging.views import list_view
 
+urlpatterns = [
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
 
