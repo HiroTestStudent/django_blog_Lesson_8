e0627ade0967d75a235d9453abd265afbac5276b
Hiroyuki Takechi <hiroyukitakechi@gmail.com>
2019-09-08 16:18:42 -0700

    final commit

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -4,7 +4,22 @@ from blogging.models import Post, Category
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInlineAdmin(admin.TabularInline):
+
+    model = Category.posts.through
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+
+    inlines = [CategoryInlineAdmin, ]
 
 
+class CategoryAdmin(admin.ModelAdmin):
+
+    exclude = ('posts', )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+# Note: I found the similar example on this website: https://cewing.github.io/training.codefellows/lectures/day27/django_admin.html
