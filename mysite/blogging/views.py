224ede79e36aecf0bd3506fe1e09cdf96f598109
Hiroyuki Takechi <hiroyukitakechi@gmail.com>
2019-09-14 20:09:12 -0700

    Lesson 8 Django ModelForm Addon

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from django.template import loader
 
 from blogging.models import Post
+
+from django import forms
+
+from django.utils import timezone
+
+from blogging.forms import MyCommentForm
 
 
 def stub_view(request, *args, **kwargs):
@@ -43,6 +49,23 @@ def detail_view(request, post_id):
         raise Http404
     context = {'post': post}
     return render(request, 'blogging/detail.html', context)
+
+
+def add_model (request):
+
+    if request.method == "POST":
+        form = MyCommentForm(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.timestamp = timezone.now()
+            model_instance.save()
+            return redirect('/')
+
+        else:
+
+            form = MyCommentForm()
+
+            return render(request, "blogging/my_template.html", {'form': form})
 
 
 
